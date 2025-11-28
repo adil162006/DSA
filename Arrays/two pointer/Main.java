defe910471f4370d5bf176da62a70b633d039777
Mohd Adil <shaikhmdadil191@gmail.com>
2025-11-28 19:16:44 +0530

    two pointer lecture 3

--- Arrays/two pointer/Main.java
+++ Arrays/two pointer/Main.java
@@ -1,3 +1,4 @@
+import java.util.*;
 public class Main {
         public static int[] twoSum(int[] numbers, int target) {
             int lp = 0;
@@ -14,7 +15,7 @@ public class Main {
             }
             return new int[] {};
         }
-    public int removeDuplicates(int[] nums) {
+    public static int removeDuplicates(int[] nums) {
         int count=1;
         int i=0;
         int j=1;
@@ -30,9 +31,68 @@ public class Main {
         return count;
 
     }
+    public int[] sortedSquares(int[] nums) {
+        ArrayList<Integer>pos=new ArrayList<>();
+        ArrayList<Integer>neg=new ArrayList<>();
+        for(int num:nums){
+            if(num<0){
+                neg.add(num);
+            }else{
+                pos.add(num);
+            }
+        }
+        //case 1:all array  elements are negative
+        if(pos.size()==0){
+            for(int i =0;i<neg.size();i++){
+                neg.set(i,neg.get(i)*neg.get(i));
+            }
+            Collections.reverse(neg);
+
+            return neg.stream().mapToInt(Integer::intValue).toArray();
+        }
+        //    case 2 : all are positive
+        if(neg.size()==0){
+            for(int i =0;i<pos.size();i++){
+                pos.set(i,pos.get(i)*pos.get(i));
+            }
+
+            return pos.stream().mapToInt(Integer::intValue).toArray();
+        }
+        //    case 3 bot negative and positive
+        int n1 = neg.size();
+        int n2 = pos.size();
+        int [] res = new int[n1+n2];
+        int i =0;
+        int j=0;
+        int id=0;
+        //squaring
+        //squaring negative
+        for (int k=0;k<n1;k++){
+            neg.set(k,neg.get(k)*neg.get(k));
+        }
+        Collections.reverse(neg);
+        //squaring positive
+        for (int k=0;k<n2;k++){
+            pos.set(k,pos.get(k)*pos.get(k));
+        }
+        while(i<n1&&j<n2){
+            if(neg.get(i)<=pos.get(j)){
+                res[id++]=neg.get(i++);
+            }else{
+                res[id++]=pos.get(j++);
+            }
+        }
+        while(i<n1){
+            res[id++]=neg.get(i++);
+        }
+        while(j<n2){
+            res[id++]=pos.get(j++);
+        }
+        return res;
+    }
     public static void main(String[] args) {
-        int arr[] = {1,2,3,4,5,6};
+        int arr[] = {1,2,2,2,3,3,4,5,6};
         int tar = 11;
-        System.out.println(twoSum(arr,tar));
+        System.out.println(removeDuplicates(arr));
     }
 }
