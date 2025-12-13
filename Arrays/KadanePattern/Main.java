4ae0dd35a3c5e253dfa41be0ce5d40748840b68e
Mohd Adil <shaikhmdadil191@gmail.com>
2025-12-13 10:59:30 +0530

    kadanes pattern last lecture

--- Arrays/KadanePattern/Main.java
+++ Arrays/KadanePattern/Main.java
@@ -44,7 +44,46 @@ public class Main {
 
             return res;
         }
+    public int maximumSum(int[] arr) {
+        int nodelete = arr[0];
+        int onedelete = Integer.MIN_VALUE;
+        int result = arr[0];
+        for (int i=1;i<arr.length;i++){
+            int prevnodelete = nodelete;
+            int prevonedelete =onedelete;
+            nodelete = Math.max(arr[i],nodelete+arr[i]);
+            int v2;
+            if(prevonedelete== Integer.MIN_VALUE){
+                v2 = arr[i];
+            }else{
+                v2 = prevonedelete+arr[i];
+            }
+            onedelete = Math.max(v2,prevnodelete);
+            result = Math.max(result,Math.max(onedelete,nodelete));
+        }
+        return result;
+    }
+    public int maxSubarraySumCircular(int[] arr) {
+        int sum=0;
+        for (int i =0;i<arr.length;i++){
+            sum+=arr[i];
+        }
 
+        int maxres=arr[0];
+        int minres=arr[0];
+        int maxbest=arr[0];
+        int minbest=arr[0];
+        for(int i=1;i<arr.length;i++){
+            maxbest = Math.max(arr[i],arr[i]+maxbest);
+            minbest = Math.min(arr[i],arr[i]+minbest);
+            minres = Math.min(minres,minbest);
+            maxres = Math.max(maxres,maxbest);
+        }
+        if(maxres<0) return maxres;
+        int CircularSum = sum-minres;
+
+        return Math.max(CircularSum,maxres);
+    }
     public static void main(String[] args) {
 
     }
