78b72ec7fc1fc72f959cc2bdc4e09c3d9e7d56cf
Mohd Adil <shaikhmdadil191@gmail.com>
2026-03-29 11:34:30 +0530

    added code for door choce and letter combination

--- Backtracking/Main.java
+++ Backtracking/Main.java
@@ -35,12 +35,73 @@ public class Main {
         return;
     }
 
+
+    public static void doorChoice(int n , int [] arr , int idx , List<String>temp){
+        //base case
+        if(idx==n){
+            System.out.println(temp);
+            return;
+        }
+
+        //left
+        temp.add("left");
+        doorChoice(n,arr,idx+1,temp);
+        temp.removeLast();
+
+        //right
+        temp.add("rigth");
+        doorChoice(n,arr,idx+1,temp);
+        temp.removeLast();
+
+
+        return;
+    }
+    static  HashMap<Character,String>f=new HashMap<>();
+    public static List<String> letterCombinations(String digits) {
+        f.put('2',"abc");
+        f.put('3',"def");
+        f.put('4',"ghi");
+        f.put('5',"jkl");
+        f.put('6',"mno");
+        f.put('7',"pqrs");
+        f.put('8',"tuv");
+        f.put('9',"wxyz");
+
+        List<String>res = new ArrayList<>();
+        StringBuilder sb = new StringBuilder("");
+        createCombination(digits,digits.length(),0,sb,res);
+        return res;
+    }
+    static void createCombination(String s,int n , int idx,StringBuilder diary , List<String>res){
+        if(idx==n){
+            res.add(diary.toString());
+            return;
+        }
+        String choice = f.get(s.charAt(idx));
+
+        //all choice
+        for(int j=0;j<choice.length();j++){
+            diary.append(choice.charAt(j));
+            createCombination(s,n,idx+1,diary,res);
+            diary.deleteCharAt(diary.length()-1);
+        }
+        return;
+
+    }
+
+
     public static void main(String[] args) {
         int[] arr = {1, 2, 3};
-        List<Integer> temp = new ArrayList<>();
+        List<String> temp = new ArrayList<>();
 
 //        generateSubsets(arr, arr.length, 0, temp);
-        StringBuilder sb = new StringBuilder();
-        generateParentheses(0,0,3,sb);
+//        StringBuilder sb = new StringBuilder();
+//        generateParentheses(0,0,3,sb);
+
+//        doorChoice(arr.length,arr ,0,temp);
+        String digits = "223"; // test input
+        List<String> result = letterCombinations(digits);
+
+        System.out.println(result);
     }
 }
